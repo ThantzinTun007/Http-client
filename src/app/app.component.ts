54b6beb5439d7630031674d71e5d992ef7590747
ThantzinTun007 <tzt.thantzintun2022@gmail.com>
2024-01-29 20:11:47 +0630

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from './post.model';
+import { PostsService } from './posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-http-client';
+export class AppComponent implements OnInit, OnDestroy {
+  loadedPosts: Post[] = [];
+  isFetching = false;
+  error = '';
+  private errorSub = new Subscription();
+  constructor(private http: HttpClient, private postsService: PostsService) {
+
+  }
+  ngOnInit(): void {
+    this.errorSub = this.postsService.error.subscribe((errorMessage) => {
+      this.error = errorMessage;
+    });
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe((posts) => {
+        this.isFetching = false;
+        this.loadedPosts = posts;
+      },
+      (error) => {
+        this.error = error.message;
+      }
+    );
+  }
+  onCreatePost(postData: Post) {
+    this.postsService.createAndStorePost(postData.title, postData.content);
+  }
+  onFetchPosts() {
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(
+      (all) => {
+        this.isFetching = false;
+        this.loadedPosts = all;
+      },
+      (error) => {
+        this.error = error.message;
+        console.log(error);
+      }
+    );
+  }
+  onClearPosts() {
+    this.postsService.deletePosts().subscribe(() => {
+      // this.loadedPosts.shift();
+      this.loadedPosts = [];
+    });
+  }
+
+  onHandleError(){
+    this.error = '';
+  }
+
+  ngOnDestroy(): void {
+    this.errorSub.unsubscribe();
+  }
 }
---
+++ src/app/posts.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from './post.model';
+import { Subject, catchError, map, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostsService {
+  error = new Subject<string>();
+  constructor(private http: HttpClient) {
+
+  }
+  
+  createAndStorePost(title: string, content: string) {
+    const postData: Post = { title: title, content: content };
+    this.http.post<{ name: string }>('https://http-client-fb407-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json',postData)
+      .subscribe(
+        (responseData) => {
+          console.log(responseData);
+        },
+        (error) => {
+          this.error.next(error.message);
+        }
+      );
+  }
+
+  fetchPosts() {
+    return this.http.get<{ [key: string]: Post }>('https://http-client-fb407-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json')
+      .pipe(
+        map((responseData) => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        }),catchError(errorRes => {
+          return throwError(errorRes);
+        })
+      );
+  }
+
+
+  deletePosts() {
+    return this.http.delete(
+      'https://http-client-fb407-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json'
+    );
+  }
+}
